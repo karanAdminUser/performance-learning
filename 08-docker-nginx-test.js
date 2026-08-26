fe90fcd4177410c54e0251b925ec134dc0fde2bc
Aman <safefoods247@gmail.com>
2026-08-26 13:29:51 -0400

    Add local obersevability stack and Docker load test

---
+++ 08-docker-nginx-test.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+    vus: 5,
+    duration: '15s',
+
+    thresholds: {
+        http_req_duration: ['p(95)<200'],
+        http_req_failed: ['rate<0.01'],
+    },
+};
+
+export default function () {
+
+    const response = http.get('http://localhost:8080');
+
+    check(response, {
+        'status is 200': (r) => r.status === 200,
+    });
+}
